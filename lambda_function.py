1bd5f0f89fcdb7a1acd32eb27188279c61ac2d07
trevorc21 <trevorc212@gmail.com>
2019-04-12 00:48:40 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -23,14 +23,17 @@ def lambda_handler(event, context):
         guild=''
         alliance=''
         guildname=''
+        lastkillfame=0
         for albionplayer in rawusers["players"]:
-            if (albionplayer['Name'].lower() == name.lower()) and (albionplayer['KillFame'] != 0):
+            if (albionplayer['Name'].lower() == name.lower()) and (albionplayer['KillFame'] >= lastkillfame):
+                lastkillfame=albionplayer['KillFame']
                 guild = albionplayer['GuildId']
                 guildname=albionplayer['GuildName']
-                try:
-                    alliance =get_guildname(albionplayer['GuildId'])['AllianceId']
-                except:
-                    alliance=albionplayer['AllianceId']
+                if albionplayer['GuildId'] != "":
+                    try:
+                        alliance =get_guildname(albionplayer['GuildId'])['AllianceId']
+                    except:
+                        alliance=albionplayer['AllianceId']
         return {"guildid":guild, "guildname":guildname, "alliance":alliance}
     def update_player(id,playerdata):
         conn = engine.connect()
@@ -49,6 +52,7 @@ def lambda_handler(event, context):
         return rawplayers
 
     players=get_waitingforverify()
+    print (players)
     for player in players:
         playerdata=get_playerguild(player['albionname'])
         update_player(player['id'],playerdata)
