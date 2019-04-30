9c33f3d2a8876ce9edd2fdbade03b20a508e2e05
trevorc21 <trevorc212@gmail.com>
2019-04-29 21:46:38 -0400

    update

--- lambda_function.py
+++ lambda_function.py
@@ -7,16 +7,62 @@ from sqlalchemy import select
 from sqlalchemy.sql import func
 import os
 db = os.environ['db']
-
+access_token = os.environ['access_token']
 
 def lambda_handler(event, context):
+    headers = {"Authorization": "Bot " + access_token}
     engine = create_engine(db)
     meta = MetaData(engine, reflect=True)
     table = meta.tables['user']
+
+    def post_accept(id):
+        requests.put("https://discordapp.com/api/guilds/200746010102726657/members/"+str(id)+"/roles/530223486694719558",data=None, headers=headers)
+        requests.put("https://discordapp.com/api/guilds/200746010102726657/members/"+str(id)+"/roles/"+guildrole(playerdata['guildid']),data=None, headers=headers)
+        guildtag=getguildtag(playerdata['guildid'])
+        requests.patch("https://discordapp.com/api/guilds/200746010102726657/members/"+str(id),data=None, json={'nick':'['+guildtag+'] '+player['albionname']}, headers=headers)
+        return
     def get_guildname(id):
-        # Return Just Player Information
         r = requests.get('https://gameinfo.albiononline.com/api/gameinfo/guilds/'+id)
         return r.json()
+
+
+    def createguildtag(guildname):
+        try:
+            return guildname[:4].upper()
+        except:
+            return 'NONE'
+
+
+    def getguildtag(guildid):
+        conn = engine.connect()
+        result = conn.execute("Select tag from guilds where guildid = '"+guildid+"'").fetchone()
+        conn.close()
+        if result:
+            return result[0]
+
+        return 'NONE'
+
+
+    def guildrole(guildid):
+        conn = engine.connect()
+        result = conn.execute("Select discordid from guilds where guildid = '"+guildid+"'").fetchone()
+ 
+        if result:
+            conn.close()
+            return result[0]
+        else:
+            if not guildid:
+                return '530223486694719558'
+            guildinfo =get_guildname(guildid)
+
+            post_data = {"name": guildinfo['Name'], "mentionable" : True}
+            headers = {"Authorization": "Bot " + access_token}
+            response=requests.post("https://discordapp.com/api/guilds/200746010102726657/roles",data=None,json=post_data, headers=headers).json()
+            engine.execute("INSERT INTO `guilds` (guildid, discordid,guildname,tag) VALUES ('{0}','{1}','{2}','{3}' )".format(guildid,response['id'],guildinfo['Name'],createguildtag(guildinfo['Name'])))
+            conn.close()
+            return response['id']
+
+
     def get_playerguild(name):
         #Returns Guild and Alliance info. This also checks for empty users as there is a bug that someusers are listed multiple times but these 'Clones' only have 0 Fame
         rawusers = requests.get('https://gameinfo.albiononline.com/api/gameinfo/search?q='+name).json()
@@ -35,6 +81,8 @@ def lambda_handler(event, context):
                     except:
                         alliance=albionplayer['AllianceId']
         return {"guildid":guild, "guildname":guildname, "alliance":alliance}
+
+
     def update_player(id,playerdata):
         conn = engine.connect()
         ins = table.update().where(table.c.id==id).values(
@@ -42,15 +90,27 @@ def lambda_handler(event, context):
             guildname=playerdata['guildname'], \
             allianceid=playerdata['alliance'])
         conn.execute(ins)
+        if playerdata['alliance']=='hRqowi9bTw6o44R0bsmIUw':
+            try:
+                post_accept(id)
+                ins = table.update().where(table.c.id==id).values(
+                    verified=True)
+                conn.execute(ins)
+            except:
+                print("Failed")
         print('Updated '+ str(id))
         conn.close()
         return
+
+
     def get_waitingforverify():
         conn = engine.connect()
         rawplayers = conn.execute(select([table]).where(table.c.verified == False)).fetchall()
         conn.close()
         return rawplayers
 
+
+ 
     players=get_waitingforverify()
     print (players)
     for player in players:
